0db3a894bef36ac973cdac9c7eec9e4619910d5a
Arjun V Ajay <arjunvajay@.com>
2020-08-12 13:16:45 +0530

    Updating permissions

--- jenkins/test/mvn-test.sh
+++ jenkins/test/mvn-test.sh
@@ -2,5 +2,7 @@
 echo "************************************"
 echo "******** Testig the Code ***********"
 echo "************************************"
+
+WORKSPACE=/var/lib/jenkins/workspace/jenkins-pipeline
 #docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine mvn test
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
